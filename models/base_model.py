2b133d9f2e6a4fdfe7b3f1d29c809b17c8a7bc70
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2023-05-01 20:06:40 +0000

    AirBnb Clone

--- models/base_model.py
+++ models/base_model.py
@@ -31,18 +31,18 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    value = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
             if "created_at" not in kwargs:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
             if "updated_at" not in kwargs:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
