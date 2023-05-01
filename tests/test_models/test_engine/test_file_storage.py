7b09772b69fa1c8ab99516bfd53756b1de9250d1
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2023-05-01 20:17:40 +0000

    AirBnb Clone

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,6 +31,7 @@ class test_fileStorage(unittest.TestCase):
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
+        temp = obj = None
         for obj in storage.all().values():
             temp = obj
         self.assertTrue(temp is obj)
@@ -67,6 +68,9 @@ class test_fileStorage(unittest.TestCase):
         storage.reload()
         for obj in storage.all().values():
             loaded = obj
+            break
+        else:
+            self.fail("No objects loaded from storage")
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
     def test_reload_empty(self):
@@ -98,6 +102,7 @@ class test_fileStorage(unittest.TestCase):
         """ Key is properly formatted """
         new = BaseModel()
         _id = new.to_dict()['id']
+        temp = None
         for key in storage.all().keys():
             temp = key
         self.assertEqual(temp, 'BaseModel' + '.' + _id)
